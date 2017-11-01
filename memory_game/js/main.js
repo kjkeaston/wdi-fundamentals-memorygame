803710aa60138602c2b15dc44acd8c8dec2fd1d1
Kyle Easton <kyleeaston@Kyles-Air.att.net>
2017-10-31 19:20:57 -0700

    created game logic unit 9

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -10,31 +10,49 @@ console.log("User flipped " + cardThree);*/
 
 
 var cards = ["queen", "queen", "king", "king"];
-
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match! You won");
-	} else {
-		alert("Not a match. Sorry, try again.");
+			alert("You found a match! You won!");
+		} else {
+			alert("Not a match. Sorry, try again.");
+		};
 	};
-}
+
+var flipCard = function (cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	};
+};
+
+flipCard(0);
+flipCard(2);
 
 
 
+//
+//
+//following code from Unit 8. Removed in Unit 9 Assignment 30%
+	/*
+	var cardOne = cards[0];
+	cardsInPlay.push(cardOne);
+	console.log("User flipped " + cardOne);
 
+	var cardTwo = cards[3];
+	cardsInPlay.push(cardTwo);
+	console.log("User flipped " + cardTwo);
 
-
-
-
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match! You won");
+		} else {
+			alert("Not a match. Sorry, try again.");
+		};
+		*/
+//
+//
 // alert("You found a match! You won!");
 // alert("Not a match. You lose!");
